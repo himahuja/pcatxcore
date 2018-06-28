62c35bc8cec3a993af11576e6539774fe55d50e2
babahooja <babahooja@gmail.com>
2018-06-27 22:48:34 -0700

    Completed the parser for 'Chemical Watch', Base Build

--- parser.py
+++ parser.py
@@ -1,3 +1,7 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
 # used for parsing the HTML contents on webscript
 import urllib.request
@@ -7,10 +11,8 @@ import pandas as pd
 import os
 # for getting directory locations
 
-
-
-
 def newsparser(url):
+
     """
     Parses the URL passed as an argument and returns a Pandas DF. It also saves a CSV file.
     Keyword arguments:
@@ -20,55 +22,41 @@ def newsparser(url):
     page = urllib.request.urlopen(url) #opens the URL page
     content = page.read() # reads the content of the page
     soup = BeautifulSoup(content, 'html.parser')
+    article = []
+    for i in soup.select("section > p"):
+        article.append(i.text)
+    # articles = soup.find_all('main', text=True)
+    # for article in articles:
+    #     print(article.contents)
+    data = (soup.select("section > h2")[0].text, article)
     #creates the soup out of the content, so the attributes can be processed
-    heads = soup.find_all('span', {'itemprop': 'headline'})
-
-    """
-    Takes the input url for processing and returns a csv of the following forrmat:
-    ['headline', 'article', 'author', 'date', 'source', 'image']
-    where image contains the whole image source
-    source contains the articles where the inshorts article is referenced from.
-    Returns a PANDAS dataframe
-    """
+    # articles = soup.find_all('div', {'itemprop': 'articleBody'})
+    # authors = soup.find_all('span', {'class': 'author'})
+    # dates = soup.find_all('span', {'clas':'date'})
+    # sources = soup.find_all('a', {'class':'source'})
+    # images = soup.find_all('div', {'class': 'news-card-image'})
+    # data = zip(heads, articles, authors, dates, sources, images)
+    # stored = []
+    # cols = ['headline', 'articleBody', 'author', 'date', 'source', 'image']
+    # for head, article, author, date, source, image in data:
+    #     stored.append([head.get_text(), article.get_text(), author.get_text(), date.get_text(), source['href'], image])
+    # df = pd.DataFrame(stored, columns = cols)
+    # return df
+    return data
 
 # neighbourhood words
-def search(text,n):
-    """Searches for text, and retrieves n words either side of the text, which are retuned seperatly"""
-    word = r"\W*([\w]+)"
-    groups = re.search(r'{}\W*{}{}'.format(word*n,'place',word*n), text).groups()
-    return groups[:n],groups[n:]
-
-    articles = soup.find_all('div', {'itemprop': 'articleBody'})
-    authors = soup.find_all('span', {'class': 'author'})
-    dates = soup.find_all('span', {'clas':'date'})
-    sources = soup.find_all('a', {'class':'source'})
-    images = soup.find_all('div', {'class': 'news-card-image'})
-    data = zip(heads, articles, authors, dates, sources, images)
-    stored = []
-    cols = ['headline', 'articleBody', 'author', 'date', 'source', 'image']
-    for head, article, author, date, source, image in data:
-        stored.append([head.get_text(), article.get_text(), author.get_text(), date.get_text(), source['href'], image])
-    df = pd.DataFrame(stored, columns = cols)
-    return df
-
-def newsparser2(url):
-        """
-        Takes the input url for processing and returns a csv of the following format:
-        text = ['headline' + 'article']
-        """
-        page = urllib.request.urlopen(url)
-        content = page.read()
-        soup = BeautifulSoup(content, 'html.parser')
-        heads = soup.find_all('span', {'itemprop': 'headline'})
-        stored = []
-        for head in heads:
-            stored.append(head.get_text())
-        return stored
+# def search(text,n):
+#     """Searches for text, and retrieves n words either side of the text, which are retuned seperatly"""
+#     word = r"\W*([\w]+)"
+#     groups = re.search(r'{}\W*{}{}'.format(word*n,'place',word*n), text).groups()
+#     return groups[:n],groups[n:]
 
 if __name__ == "__main__":
+    store = []
     # sample URL for testing.
-    url = 'https://news.thomasnet.com/imt/2000/11/13/changing_the_im'
-
+    # url = 'https://news.thomasnet.com/imt/2000/11/13/changing_the_im'
+    url = 'https://events.chemicalwatch.com/68002/from-raw-material-to-final-product'
+    store.append(newsparser(url))
 
 
 # df = newsparser('file:///Applications/XAMPP/xamppfiles/htdocs/NIS/AI-to-News-in-Shorts/data/7_inshorts.html')
