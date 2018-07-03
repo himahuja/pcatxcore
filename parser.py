58846359d558bdba38b0fcc780486993e4bdc47a
Alexander Michels <alexandercm4297@gmail.com>
2018-07-03 10:20:59 -0700

    Melody and I fixed PDF parsing :tada:

--- parser.py
+++ parser.py
@@ -27,14 +27,14 @@ def get_PDF_content(link, linkList):
     #download pdf file ,from web
     content=urlopen(link).read()
     file_name = "pdf"+str(linkList.index(link))+".pdf"
-    fout=open(os.path.join("data/source", file_name), "wb")
+    fout=open(os.path.join("data/source/", file_name), "wb")
     fout.write(content)
     fout.close()
 
     #convert PDF to text
     content = ""
     #load PDF into PyPDF2
-    pdf = PyPDF2.PdfFileReader(file_name)
+    pdf = PyPDF2.PdfFileReader(os.path.join("data/source/", file_name))
     #iterate pages
     for i in range(pdf.getNumPages()):
     #extract text from page and add to content
@@ -59,13 +59,13 @@ def parser(linkList):
             try:
                 content = get_PDF_content(link, linkList)
                 file_name = "page"+str(linkList.index(link))+".txt"
-                text_list = nltk.sent_tokenize(text_from_html(html))
+                text_list = nltk.sent_tokenize(content)
                 text_file = open(os.path.join("data/sentences", file_name), "w")
                 for i in range(len(text_list)):
                     text_file.write(text_list[i].strip() + "\n")
                 text_file.close()
-            except:
-                pass
+            except Exception as e:
+                print(e)
         print("...{:.2f}% done, processing link {}".format(((linkList.index(link)+1)/len(linkList))*100,linkList.index(link)))
 
 def main():
