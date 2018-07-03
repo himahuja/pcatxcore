7465f7e53adad0c0e807e9d47a2d846698eb1240
Melody <ls4081@nyu.edu>
2018-07-02 20:38:06 -0700

    parser revision
    this parser is able to handle pdf url now

--- parser.py
+++ parser.py
@@ -1,26 +1,57 @@
 from bs4 import BeautifulSoup
+from bs4.element import Comment
 import urllib.request
+from urllib.request import urlopen
+import os
+import webbrowser
+import PyPDF2
 
+ 
 def tag_visible(element):
-    #this is to determine whether the content in a tag is visible on a webpage
-    if element.parent.name in ['head', 'style', 'script', 'title', 'meta', '[document]']:
+    if element.parent.name in ['[document]', 'head', 'style', 'script', 'title', 'header', 'meta', 'footer']:
         return False
     if isinstance(element, Comment):
+        return False
+    if element.name in ['header','footer','button','nav']:
         return False
     return True
 
 def text_from_html(body):
     soup = BeautifulSoup(body, 'lxml')
     texts = soup.findAll(text=True)
-    #only extract the visible and relevant content that we care
     visible_texts = filter(tag_visible, texts)  
     return u" ".join(t.strip() for t in visible_texts)
 
+def get_PDF_content(url):
+    #download pdf file from web
+    content=urlopen(url).read()
+    file_name = "pdf"+str(linkList.index(link))+".pdf"
+    fout=open(file_name, "wb")
+    fout.write(content)
+    fout.close()
+
+    #convert PDF to text
+    content = ""
+    #load PDF into PyPDF2
+    pdf = PyPDF2.PdfFileReader(file_name)
+    #iterate pages
+    for i in range(pdf.getNumPages()):
+    #extract text from page and add to content
+        content += pdf.getPage(i).extractText() + "\n"
+        content = " ".join(content.replace("\xa0", " ").strip().split())
+    return content
+
 def parser(linkList):
-    #this is to parse a list of links to text files individually
     for link in linkList:
-        html = urllib.request.urlopen(link).read()
-        file_name = "page"+str(linkList.index(link))+".txt"
-        text_file = open(file_name, "w")
-        text_file.write(text_from_html(html))
-        text_file.close()
+        if link[-4:] != '.pdf':
+            html = urllib.request.urlopen(link).read()
+            file_name = "page"+str(linkList.index(link))+".txt"
+            text_file = open(file_name, "w")
+            text_file.write(text_from_html(html))
+            text_file.close()
+        else:
+            content = get_PDF_content(link)
+            file_name = "page"+str(linkList.index(link))+".txt"
+            text_file = open(file_name, "w")
+            text_file.write(content)
+            text_file.close()  
