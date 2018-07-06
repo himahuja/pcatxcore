04de3f542db810697d345fc7203bf71d5ac7b701
babahooja <babahooja@gmail.com>
2018-07-06 15:01:38 -0700

    Loads the webpage and finds all the search results

--- webcrawlAll.py
+++ webcrawlAll.py
@@ -7,8 +7,22 @@ from selenium.webdriver.common.keys import Keys
 if __name__ == "__main__":
     search_query = ""
     search_query.replace(" ", "+")
-    url = "https://chemicalwatch.com/search?q=" + search_query
-    driver = webdriver.Chrome()
-    driver.get(url)
+    url = "https://www.google.com/search?q=deep+learnining+nltk+hill+mo+ked"
+    # url = "https://chemicalwatch.com/search?q=" + search_query
+    driver = webdriver.Chrome('/Users/himanshuahuja/chromedriver')
+    # driver = webdriver.Chrome()
+    driver.get(url) # opens the URL
+    # search_results = driver.find_element_by_xpath("//html/body/div[@id='main']/div[@id='cnt']/div[@class='mw']/div[@id='rcnt']/div[@class='col']/div[@id='center_col']/div[@id='res']/div[@id='search']//div[@id='ires']/div[@id='rso']/div[@class='bkWMgd']/div[@class='srg']/div[@class='g']")#/div[@class='rc']/div[@class='r']")
+    search_results = []
+    search_results = driver.find_elements_by_class_name('g')
+    print(len(search_results))
+    for x in range(0,len(search_results)):
+        if search_results[x].is_displayed():
+            like[x].click()
+    # print(search_results.tag_name)
+    # print(search_results.get_attribute('value'))
+    # driver.quit()
+
+
 
 # GOING TO ADD THE httrack script here for the terminal.
