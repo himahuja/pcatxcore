ee9ff70aeb0b04462819d190844fb3d5909e80ab
Alexander Michels <alexandercm4297@gmail.com>
2018-07-14 23:06:26 -0700

    idk

--- ProfileManager.py
+++ ProfileManager.py
@@ -5,13 +5,14 @@ Created on Wed Jul 11 12:47:13 2018
 
 @author: alex
 """
+import json
 
 class Profile(object):
     
     def __init__(self, file_name):
         this = json.loads(open(file_name, "r").read())
         self.name = this['name']
-        self.10k = this['10k']
+        self.tenk = this['tenk']
         self.cik = this['cik']
         self.naics = this['naics']
         self.sic = this['sic']
@@ -26,10 +27,13 @@ class ProfileManager(object):
         self.name_cik = {}
         self.name_alias = {}
         #sic <---> naics transforms
-        self.naics_description = {}
-        self.sic_description = {}
-        self.naics_sic = {}
-        self.sic_naics = {}
+        try:
+            self.naics_description = json.loads(open("data/profilemanager/data/naics2017_to_naics2017_title.json", "r").read())
+            self.sic_description = json.loads(open("data/profilemanager/data/sic_to_description.json", "r").read())
+            self.naics_sic = json.loads(open("data/profilemanager/data/naics_to_sic.json", "r").read())
+            self.sic_naics = json.loads(open("data/profilemanager/data/sic_to_naics.json", "r").read())
+        except Exception as e:
+            print("Error reading in one of the SIC <---> NAICS Transforms " + str(e))
         
     def __contains__(self, key):
         if key in self.cik_name:
@@ -44,17 +48,21 @@ class ProfileManager(object):
     
     def __getitem(self, key):
         if key in self.cik_name:
+            pass
             #open this file, we have cik
         elif key in self.name_cik:
+            pass
             #open name_cik[key]
         else:
             for i in range(len(self.name_aliases.values())):
                 for alias in self.name_aliases.values()[i]:
                     if key == alias:
+                        pass
                         # open name_cik[name_aliases.keys()[i]]
         
     def __iter__(self):
         for cik in self.cik_name.keys():
+            pass
             #open the profile
         
     def __len__(self):
@@ -85,13 +93,16 @@ class ProfileManager(object):
         
     def get(self, key):
         if key in self.cik_name:
+            pass
             #open this file, we have cik
         elif key in self.name_cik:
+            pass
             #open name_cik[key]
         else:
             for i in range(len(self.name_aliases.values())):
                 for alias in self.name_aliases.values()[i]:
                     if key == alias:
+                        pass
                         # open name_cik[name_aliases.keys()[i]]
     
     def naics_to_description(self, naics):
@@ -108,3 +119,13 @@ class ProfileManager(object):
     
     def name_to_description(self, name):
         return self.naics_description[self.get(name).naics] + self.sic_description[self.get(name).sic]
+        
+def main():
+    pm = ProfileManager()
+    print(pm.naics_description)
+    print(pm.sic_description)
+    print(pm.naics_sic)
+    print(pm.sic_naics)
+    
+if __name__ == "__main__" :
+    main()
