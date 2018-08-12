4dd3b5fc8f3a6f1c61f04b6d45c92184b09ac9b8
Himanshu Ahuja <babahooja@gmail.com>
2018-08-11 23:13:00 -0700

    Removed deprecated modules

--- rdf4j-database/src/main/java/ChunkCommitter.java
+++ rdf4j-database/src/main/java/ChunkCommitter.java
@@ -1,48 +1,71 @@
 import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.URI;
+import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.repository.util.RDFInserter;
 import org.eclipse.rdf4j.rio.RDFHandler;
 import org.eclipse.rdf4j.rio.RDFHandlerException;
 
-class ChunkCommitter implements RDFHandler {
-  private RDFInserter inserter;
-  private RepositoryConnection conn;
-  private long count = 0L;
-  // do intermittent commit every 500,000 triples
-  private long chunksize = 500000L;
-  public ChunkCommitter(RepositoryConnection conn) {
-    inserter = new RDFInserter(conn);
-    this.conn = conn;
-  }
-  @Override
-  public void startRDF() throws RDFHandlerException {
-    inserter.startRDF();
-  }
-  @Override
-  public void endRDF() throws RDFHandlerException {
-    inserter.endRDF();
-  }
-  @Override
-  public void handleNamespace(String prefix, String uri) throws RDFHandlerException {
-    inserter.handleNamespace(prefix, uri);
-  }
-  @Override
-  public void handleStatement(Statement st) throws RDFHandlerException {
-    inserter.handleStatement(st);
-    count++;
-    // do an intermittent commit whenever the number of triples
-    // has reached a multiple of the chunk size
-    if (count % chunksize == 0) {
-      try {
-        conn.commit();
-      } catch (RepositoryException e) {
-        throw new RDFHandlerException(e);
-      }
+public class ChunkCommitter implements RDFHandler {
+
+    private final long chunkSize;
+    private final RDFInserter inserter;
+    private final RepositoryConnection conn;
+    private final URI context;
+    private final ValueFactory factory;
+
+    private long count = 0L;
+
+    public ChunkCommitter(RepositoryConnection conn, URI context, long chunkSize) {
+        this.chunkSize = chunkSize;
+        this.context = context;
+        this.conn = conn;
+        this.factory = conn.getValueFactory();
+        inserter = new RDFInserter(conn);
     }
-  }
-  @Override
-  public void handleComment(String comment) throws RDFHandlerException {
-    inserter.handleComment(comment);
-  }
+
+    public long getStatementCount() {
+        return count;
+    }
+
+    @Override
+    public void startRDF() throws RDFHandlerException {
+        inserter.startRDF();
+    }
+
+    @Override
+    public void endRDF() throws RDFHandlerException {
+        inserter.endRDF();
+    }
+
+    @Override
+    public void handleNamespace(String prefix, String uri)
+            throws RDFHandlerException {
+        inserter.handleNamespace(prefix, uri);
+    }
+
+    @Override
+    public void handleStatement(Statement st) throws RDFHandlerException {
+        if(context !=null) {
+            st = factory.createStatement(st.getSubject(), st.getPredicate(), st.getObject(), context);
+        }
+        inserter.handleStatement(st);
+        count++;
+        // do an intermittent commit whenever the number of triples
+        // has reached a multiple of the chunk size
+        if (count % chunkSize == 0) {
+            try {
+                conn.commit();
+                System.out.print(".");
+            } catch (RepositoryException e) {
+                throw new RDFHandlerException(e);
+            }
+        }
+    }
+
+    @Override
+    public void handleComment(String comment) throws RDFHandlerException {
+        inserter.handleComment(comment);
+    }
 }
